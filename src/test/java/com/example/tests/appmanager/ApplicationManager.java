1e929aa3bd411bc38fb8a7831df58b0a8cbb8169
dmitrii-aleksandrov-u <52959570+dmitrii-aleksandrov-u@users.noreply.github.com>
2020-03-09 18:52:46 +0300

    Update ApplicationManager.java

--- src/test/java/com/example/tests/appmanager/ApplicationManager.java
+++ src/test/java/com/example/tests/appmanager/ApplicationManager.java
@@ -51,7 +51,8 @@ public class ApplicationManager {
              driver = new FirefoxDriver();
 
          } else if (browser.equals(BrowserType.CHROME)) {
-          //   System.setProperty("webdriver.chrome.driver", "C:\\Tools\\Tools\\chromedriver.exe");
+             System.setProperty("webdriver.chrome.driver", "/opt/google/chrome");
+             //"C:\\Tools\\Tools\\chromedriver.exe"
              driver = new ChromeDriver();
          } else if (browser.equals(BrowserType.IE)) {
              System.setProperty("webdriver.ie.driver", "C:\\Tools\\Tools\\IEDriverServer.exe");
