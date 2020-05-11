89384871084346641833427d5854b9c8fd2cf276
dmitrii-aleksandrov-u <52959570+dmitrii-aleksandrov-u@users.noreply.github.com>
2020-05-11 18:43:35 +0300

    9

--- src/test/java/com/example/tests/test/PollCreationTests.java
+++ src/test/java/com/example/tests/test/PollCreationTests.java
@@ -10,7 +10,7 @@ public class PollCreationTests extends TestBase {
 
     app.getNavigationHelper().gotoPollListPage();
     app.getPollHelper().initPollCreation();
-    app.getPollHelper().fillPollForm(new PollData("Тестовый опрос1", "Описание тестового опроса", "Вопрос №1", "2", "Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3", "Вопрос №2", "3", "Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3", "Вариант ответа 4"));
+    app.getPollHelper().fillPollForm(new PollData("Тестовый опрос", "Описание тестового опроса", "Вопрос №1", "2", "Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3", "Вопрос №2", "3", "Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3", "Вариант ответа 4"));
     app.getPollHelper().submitPollCreation();
     app.getPollHelper().checkElements();
   }
