5eef7890db02ee7517e2c170e105bfe090b72c28
dmitrii-aleksandrov-u <52959570+dmitrii-aleksandrov-u@users.noreply.github.com>
2020-05-12 16:06:44 +0300

    Update PollHelper.java

--- src/test/java/com/example/tests/appmanager/PollHelper.java
+++ src/test/java/com/example/tests/appmanager/PollHelper.java
@@ -119,9 +119,9 @@ public class PollHelper extends HelperBase {
 
     public void deletePoll() throws InterruptedException {
       click(By.linkText("Удалить"));
-        Thread.sleep(1000)
+        Thread.sleep(1000);
       assertTrue(closeAlertAndGetItsText().matches("^Вы действительно хотите удалить этот элемент[\\s\\S]$"));
-        Thread.sleep(1000)
+        Thread.sleep(1000);
     }
 
     public void selectPoll() {
