91420777c087a972d6f7f49ebbfbb087d599a350
dmitrii-aleksandrov-u <52959570+dmitrii-aleksandrov-u@users.noreply.github.com>
2020-05-12 18:38:58 +0300

    17

--- src/test/java/com/example/tests/test/PollModificationTests.java
+++ src/test/java/com/example/tests/test/PollModificationTests.java
@@ -9,7 +9,7 @@ public class PollModificationTests extends TestBase {
         app.getNavigationHelper().gotoPollListPage();
         app.getPollHelper().selectPoll();
         app.getPollHelper().initPollModification();
-        app.getPollHelper().modifyPollForm(new PollData("Тестовый опрос", "Описание тестового опроса ред.", "Вопрос №1 ред.", "2", "Вариант ответа 1 ред.", "Вариант ответа 2 ред.", "Вариант ответа 3 ред.", "Вопрос №2 ред.", "3", "Вариант ответа 1 ред.", "Вариант ответа 2 ред.", "Вариант ответа 3 ред.", "Вариант ответа 4 ред."));
+        app.getPollHelper().modifyPollForm(new PollData("Тестовый опрос ред.", "Описание тестового опроса ред.", "Вопрос №1 ред.", "2", "Вариант ответа 1 ред.", "Вариант ответа 2 ред.", "Вариант ответа 3 ред.", "Вопрос №2 ред.", "3", "Вариант ответа 1 ред.", "Вариант ответа 2 ред.", "Вариант ответа 3 ред.", "Вариант ответа 4 ред."));
         app.getPollHelper().submitPollModification();
     }
 
