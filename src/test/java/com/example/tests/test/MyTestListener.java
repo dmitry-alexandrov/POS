f2a284c35a7c5fb0948e84c4b8b23312e85c7b25
dmitrii-aleksandrov-u <52959570+dmitrii-aleksandrov-u@users.noreply.github.com>
2020-05-14 22:44:58 +0300

    36

--- src/test/java/com/example/tests/test/MyTestListener.java
+++ src/test/java/com/example/tests/test/MyTestListener.java
@@ -2,13 +2,13 @@ package com.example.tests.test;
 
 import com.example.tests.appmanager.ApplicationManager;
 //import io.qameta.allure.Attachment;
-import io.qameta.allure.Attachment;
+//import io.qameta.allure.Attachment;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-//import ru.yandex.qatools.allure.annotations.Attachment;
+import ru.yandex.qatools.allure.annotations.Attachment;
 
-//import ru.yandex.qatools.allure.annotations.Attachment;
+
 
 
 public class MyTestListener implements ITestListener {
